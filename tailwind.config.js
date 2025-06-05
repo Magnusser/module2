7458d2e51a9734df6307f22f3cd3972a62349b8f
Magnusser <magnus.roland.raadla@gmail.com>
2025-06-06 00:29:52 +0300

    almost finished with item page

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,6 @@ export default {
       colors: {
         primary: '#CB2729',
         secondary: '#770000',
-        third: '#BD3435',
         golden: '#D89C00',
         darkness: '#151415',
         lightness: '#FFFAED',
