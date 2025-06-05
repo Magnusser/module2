0b52ff237fcd02dc10bd6afa938b19844ba36fff
Magnusser <magnus.roland.raadla@gmail.com>
2025-06-05 17:42:52 +0300

    DID A LOT

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,12 @@ export default {
   theme: {
     extend: {
       colors: {
-        'primary': '#CB2729',
-        'secondary': '#770000',
-        'third': '#BD3435',
-        'golden': '#D89C00',
-        'darkness': '#151415',
-        'lightness': '#FFFAED',
+        primary: '#CB2729',
+        secondary: '#770000',
+        third: '#BD3435',
+        golden: '#D89C00',
+        darkness: '#151415',
+        lightness: '#FFFAED',
       },
       fontFamily: {
         khula: ['Khula', 'sans'],
